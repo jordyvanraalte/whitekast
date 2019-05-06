d809e6fe917fb02104e242e0c0167b6d61887494
Simon Szydlik <daningteen@gmail.com>
2019-05-06 15:42:13 +0200

    [Added] Basic Game Setup

--- whitekast/whitekast/Game.cpp
+++ whitekast/whitekast/Game.cpp
@@ -1,12 +1,38 @@
 #include "pch.h"
+#include "wtypes.h"
 #include "Game.h"
+#include "Player.h"
+#include <iostream>
+using namespace std;
 
+Player player;
 
-Game::Game()
-{
+Game::Game(const char* title, int argc, char* argv[]) {
+	int horizontal = 0;
+	int vertical = 0;
+	getDesktopResolution(horizontal, vertical);
 }
 
-
-Game::~Game()
-{
+Game::~Game() {
 }
+
+void Game::handleEvents() {
+
+}
+
+void Game::update()	{
+
+}
+
+void Game::getDesktopResolution(int& horizontal, int& vertical) {
+	RECT desktop;
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+	horizontal = desktop.right;
+	vertical = desktop.bottom;
+}
+
+void Game::clean() {
+
+}
+
