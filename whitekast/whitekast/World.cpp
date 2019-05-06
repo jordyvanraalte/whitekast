dfdcfbb33c481e07b51243f13be04c8939cbe2f9
Whisper <gewoonjvr@gmail.com>
2019-05-06 15:43:00 +0200

    [Added] begin of world class

--- whitekast/whitekast/World.cpp
+++ whitekast/whitekast/World.cpp
@@ -1,12 +1,45 @@
 #include "pch.h"
 #include "World.h"
+#include <GL/freeglut.h>
 
 
-World::World()
-{
+
+static World* world;
+
+World::World(const char* titel, int argc, char *argv[], int width, int height) {
+
+	//window inits
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(width, height);
+	glutCreateWindow(titel);
+	world = this;
+
+	//functions inits
+	glutIdleFunc([]() {world->idle(); });
+	glutDisplayFunc([]() {world->display(); });
+	glutMainLoop();
 }
 
 
-World::~World()
-{
+World::~World() {
+	delete world;
 }
+
+World* World::getWorld(const char* titel, int argc, char* argv[], int width, int height)
+{
+	if (world != NULL) {
+		world = new World(titel, argc, argv, width, height);
+	}
+	return world;
+}
+
+void World::idle(void){
+}
+
+void World::display(void){
+}
+
+void World::reshape(int width, int height){
+}
+
--- whitekast/whitekast/World.h
+++ whitekast/whitekast/World.h
@@ -1,8 +1,17 @@
 #pragma once
+
 class World
 {
-public:
-	World();
+private:
+	World(const char* titel, int argc, char *argv[], int width, int height);
 	~World();
+
+	void idle(void);
+	void display(void);
+	void reshape(int width, int height);
+
+public:
+	World* getWorld(const char* titel, int argc, char* argv[], int width, int height);
 };
+
 
