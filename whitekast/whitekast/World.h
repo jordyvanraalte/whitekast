ae0865610693be88c8d57e88666d29f6d729cf1c
vissnor <Daimvis@outlook.com>
2019-05-13 12:13:04 +0200

    [Added] keyboard, reshape and constructor

--- whitekast/whitekast/World.h
+++ whitekast/whitekast/World.h
@@ -7,12 +7,15 @@ class World
 {
 
 public:
-	World();
+	World(int horizontal, int vertical);
 	~World();
 
+	World* createWorld(int, int);
 	void idle(void);
 	void display(std::vector<GameObject> objects);
 	void reshape(int width, int height);
+	void keyboard(unsigned char key, int mouseX, int mouseY);
+	void keyboardUp(unsigned char key, int mouseX, int mouseY);
 	static World* getWorld();
 };
 
