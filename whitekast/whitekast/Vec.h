fa57528064c57857724ae6a5ae7e3a1785475f6e
Simon Szydlik <daningteen@gmail.com>
2019-05-27 09:27:59 +0200

    [Added] Vector operator overloads

--- whitekast/whitekast/Vec.h
+++ whitekast/whitekast/Vec.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <math.h>
+
+#define PI 3.14159265
+#define DEG_TO_RAD PI / 180.0f
+
 
 class Vec3f
 {
@@ -17,6 +22,16 @@ public:
 	float& operator [](int);
 };
 
+inline Vec3f operator +(const Vec3f& lhs, const Vec3f& rhs)
+{
+	return Vec3f(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+}
+
+inline Vec3f operator -(const Vec3f& lhs, const Vec3f& rhs)
+{
+	return Vec3f(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+}
+
 class Vec2f
 {
 public:
@@ -32,5 +47,26 @@ public:
 	Vec2f(const float x, const float y);
 	Vec2f(const Vec2f& other);
 	float& operator [](int);
+
 };
+
+inline Vec2f operator +(const Vec2f& lhs, const Vec2f& rhs)
+{
+	return Vec2f(lhs.x + rhs.x, lhs.y + rhs.y);
+}
+
+inline Vec2f operator -(const Vec2f& lhs, const Vec2f& rhs)
+{
+	return Vec2f(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+inline Vec2f rotateVector(Vec2f& v, const float angle)
+{
+	auto radAngle = static_cast<float>(angle * DEG_TO_RAD);
+	return Vec2f(
+		static_cast<float>(v.x * cos(radAngle) - v.y * sin(radAngle)),
+		static_cast<float>(v.x * sin(radAngle) + v.y * cos(radAngle))
+	);
+}
+
 
